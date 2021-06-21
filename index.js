58741e79e9a9571e35d2a9655920f6a99c157ddb
ketevan <ketevan.mdzinarishvili.1@btu.edu.ge>
2021-06-21 17:16:52 +0400

    added response headers that allow for the app post call to work (look into further)

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 routes(app);
 //crud
